ddcdd01fe5e7f340c907ca9c9e14778dda3e232d
subham2015 <pudi.subham@gmail.com>
2018-06-30 20:47:24 -0400

    Adding the initial Login Page form with check for valid email and password

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+const HomePage = () =>(
+
+<div>
+
+<h1>Home Page</h1> 
+
+<NavLink to ="/login"> Login </NavLink>
+</div>
+	);
+
+export default HomePage;
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import LoginForm from '../forms/LoginForm';
+
+class LoginPage extends React.Component {
+
+
+    submit = (data) => {
+
+
+    	console.log(data);
+    };
+	render() {
+
+		return(
+          <div>
+          <h1> Login Page </h1>
+          <LoginForm submit={this.submit}/>                
+          </div>
+	          );
+	}
+}
+
+export default LoginPage;
